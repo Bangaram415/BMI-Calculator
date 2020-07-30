7c985102638295381d4b644a0e59ff64b9c20bfc
bangaram415 <pulla.bangaram4@gmail.com>
2020-07-30 13:04:01 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/screens/input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFFfe9801),
+        scaffoldBackgroundColor: Color(0xFF7fa998),
+      ),
+      home: InputPage(),
+    );
+  }
+}
